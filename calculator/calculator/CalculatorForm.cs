7acc8bbe1e662694adbf6758a16d0fc2de46d153
Audiosyncrasy <jer.killer2002@gmail.com>
2022-04-07 08:49:22 -0700

    Revert "Revert "Merge pull request #2 from Audiosyncrasy/feature/Concatenation_Logic""
    This reverts commit 7bedd15ac5d26f1e424d990f7cac9c4a91333fdf.

--- calculator/calculator/CalculatorForm.cs
+++ calculator/calculator/CalculatorForm.cs
@@ -2,6 +2,10 @@ namespace calculator
 {
     public partial class CalculatorForm : Form
     {
+        private string _operatorClicked = "";
+        private double _resultValue = 0;
+        private bool _endOfCalcFlag = false;
+
         public CalculatorForm()
         {
             InitializeComponent();
@@ -9,13 +13,93 @@ namespace calculator
 
         private void BtnClick(object sender, EventArgs e)
         {
+            if (_endOfCalcFlag)
+            {
+                ClearAll();
+                _endOfCalcFlag = false;
+            }
             Button btn = (Button)sender;
-            resultBox.Text += btn.Text;
+            ClearZeroAndConcatIntExceptWhenDecimal(btn.Text);
         }
 
-        public int CalcSum(int num1, int num2)
+        private void OperatorClick(object sender, EventArgs e)
+        {
+            if (_operatorClicked != "") return;
+            Button button = (Button) sender;
+            _operatorClicked = button.Text;
+
+            _resultValue = double.Parse(resultBox.Text);
+            resultTemp.Text = resultBox.Text;
+            resultBox.Text = "0";
+        }
+
+        public double CalcSum(double num1, double num2)
         {
             return num1 + num2;
+        }
+
+        public double CalcDifference(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double CalcQuotient(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        public double CalcProduct(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public void ClearZeroAndConcatIntExceptWhenDecimal(string num)
+        {
+            if (returnResultBox().Text == "0" && num != ".")
+            {
+                resultBox.Clear();
+            }
+
+            returnResultBox().Text += num;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearAll();
+        }
+
+        private void ClearAll()
+        {
+            _resultValue = 0;
+            resultBox.Text = _resultValue.ToString();
+            resultTemp.Text = "";
+        }
+
+        private void btnEquals_Click(object sender, EventArgs e)
+        {
+            var num1 = _resultValue;
+            var num2 = Double.Parse(resultBox.Text);
+            resultTemp.Text += _operatorClicked + resultBox.Text + "=";
+
+            switch (_operatorClicked)
+            {
+                case "+":
+                    resultBox.Text = CalcSum(num1, num2).ToString();
+                    break;
+                case "-":
+                    resultBox.Text = CalcDifference(num1, num2).ToString();
+                    break;
+                case "/":
+                    resultBox.Text = CalcQuotient(num1, num2).ToString();
+                    break;
+                case "*":
+                    resultBox.Text = CalcProduct(num1, num2).ToString();
+                    break;
+                default:
+                    break;
+            }
+
+            _endOfCalcFlag = true;
         }
     }
 }
